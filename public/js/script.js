5a2958112aac2084e2008d4bee293fddb001926a
Alexandros Oikonomou <alexoik2001@gmail.com>
2023-11-03 19:09:46 +0200

    completed development of battleship's main webpage

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,25 @@ menu.onclick = () => {
     navList.classList.toggle('open');
 }
 
+/* When the user clicks on the button, toggle 
+between hiding and showing the dropdown content */
+function DropDownOnClick() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+// Close the dropdown if the user clicks outside of it.
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 //Using 'ScrollReveal' by https://github.com/jlmakes/scrollreveal):
 //Creating ScrollReveal:
 const sr = ScrollReveal({
@@ -19,4 +38,5 @@ const sr = ScrollReveal({
 sr.reveal('.responsive-nav', { delay: 500, origin: 'top' });
 sr.reveal('.home-text', { delay: 1500, origin: 'top' });
 sr.reveal('.game-image', { delay: 2500, origin: 'top' });
-sr.reveal('.scroll-down', { delay: 3500, origin: 'right' });
+sr.reveal('.goto-github', { delay: 3500, origin: 'right' });
+sr.reveal('.ft', { delay: 4500, origin: 'left' });
