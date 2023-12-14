8c78a9574a462e6e10b050186e3ec27e985c8693
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-12-14 12:00:25 +0200

    Fixed DB connection for multiplayer battleship

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -2,12 +2,12 @@
     $host = 'localhost';
     $user = 'root';
     $db = 'battleshipgamedb';
-    require_once "db_upass.php";
+    require_once "lib/db_upass.php";
 
-    // $user = $DB_USER;
-    // $pass = $DB_PASS;
+    $user = $DB_USER;
+    $pass = $DB_PASS;
 
-    $mysqli = new mysqli($host, $user, '', $db);
+    $mysqli = new mysqli($host, $user, $pass, $db);
 
     if(!$mysqli) {
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
